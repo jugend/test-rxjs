4475310ae7333c73f3f0bf0d3290ab3dab947a7f
Herryanto Siatono <herryanto@gmail.com>
2018-02-16 10:54:39 +0800

    add autocomplete-rxjs

---
+++ src-client/autocomplete-rxjs.js
@@ -0,0 +1,26 @@
+import $ from 'jquery'
+import Rx from 'rxjs/Rx'
+
+const $title = $('#title')
+const $results = $('#results')
+
+const keyUp$ = Rx.Observable.fromEvent($title, 'keyup')
+const queries$ = keyUp$
+  .map(e => e.target.value)
+  .distinctUntilChanged()
+  .debounce(250)
+  .switchMap(getItems) // Old name flatMapLastest => Only get the latest query
+
+queries$.subscribe(items => {
+  $results.empty()
+  $results.append(items.map(r => $('<li />').text(r)))
+})
+
+function getItems(title) {
+  console.log(`Querying ${title}`)
+  return new Promise((resolve, reject) => {
+    window.setTimeout(() => {
+      resolve([title, 'Item 2', `Another ${Math.random()}`])
+    }, 500 + Math.random() * 5000)
+  })
+}
